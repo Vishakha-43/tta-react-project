1333f7592e21d53fe2b0723cd9ce13909a9bf3d4
Vishakha-43 <lavishkamboj60@gmail.com>
2023-09-22 16:31:17 +0530

    Completed Task

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import aboutImg from './images/mount1.jpg'
+import Title from './Title'
 
 const About = () => {
   return (
     <>
-        <h1>About Section</h1>
+        <section className="section">
+      <Title title='about' subTitle='us'/>
+
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img
+            src={aboutImg}
+            className="about-photo"
+            alt="awesome mountains"
+          />
+        </div>
+        <article className="about-info">
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+        </article>
+      </div>
+    </section>
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import homeImg from './images/mount2.jpg'
+import Title from './Title'
 const Home = () => {
   return (
-    <h1>Home Secrion</h1>
+    <>
+      <section className="section">
+      <Title title='home'/>
+
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img
+            src={homeImg}
+            className="about-photo"
+            alt="awesome mountains"
+          />
+        </div>
+        <article className="about-info">
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+        </article>
+      </div>
+    </section>
+    </>
   )
 }
 
